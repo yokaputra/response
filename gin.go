ba85e4c99eb3cb4235b3dcebded2f7f48ae3dea4
yokaputra <yokasegaraputra.023@gmail.com>
2023-10-01 14:53:52 +0700

    feat(gin): implement use response with gin

---
+++ gin.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"response/errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GinErrorResponse(c *gin.Context, err error) {
+	c.Set(errors.ErrorMessageKey, err.Error())
+	response := NewErrorResponse(err)
+	c.JSON(response.StatusCode, response)
+}
+
+func GinSuccessResponse(c *gin.Context, message string, data interface{}, statusCode ...int) {
+	response := NewSuccessResponse(message, data, statusCode...)
+	c.JSON(response.StatusCode, response)
+}
